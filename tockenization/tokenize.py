7267a76d1d905f91b119df8d0c2a0983c2377096
unknown <jjujju31@naver.com>
2020-12-16 09:49:27 +0900

    tokenized

---
+++ tockenization/tokenize.py
@@ -0,0 +1,13 @@
+import sys
+from konlpy.tag import Komoran
+
+if __name__ == "__main__":
+    kom = Komoran()
+    target_index = int(sys.argv[1]) # 1
+
+    for line in sys.stdin:
+        if line.strip() != "":
+            columns = line.strip().split('\t')
+            columns[target_index] = ' '.join(kom.morphs(columns[target_index]))
+
+        sys.stdout.write('\t'.join(columns) + "\n")
